4d74efeade95bad10c4b911001f155f1182d00fc
brietk <brietkonn@gmail.com>
2016-05-01 18:23:48 +0000

    Entity klasar

---
+++ Mooshak2_Hopur5/Models/Entities/UserAssignmetEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2_Hopur5.Models.Entities
+{
+    public class UserAssignmetEntity
+    {
+    }
+}
