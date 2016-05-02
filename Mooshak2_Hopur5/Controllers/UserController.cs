8a4263f7e9049fb2f2e598d7df0cd151836b90df
brietk <brietkonn@gmail.com>
2016-05-02 22:01:42 +0000

    Birta User úr Entiti

--- Mooshak2_Hopur5/Controllers/UserController.cs
+++ Mooshak2_Hopur5/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mooshak2_Hopur5.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,24 @@ namespace Mooshak2_Hopur5.Controllers
 {
     public class UserController : Controller
     {
+        private UserService _service = new UserService();
+
         // GET: User
         public ActionResult Index()
         {
             return View();
+        }
+
+        // GET: Course
+        public ActionResult GetUser(int? userId)
+        {
+            Models.ViewModels.UserViewModel viewModel = null;
+            if ( userId == null)
+                viewModel = _service.getUserById(3);
+            else
+                viewModel = _service.getUserById((int)userId);
+
+            return View(viewModel);
         }
     }
 }
