4492c715e9ceba833cb2f6cce8424b1fb4b90db9
Harpa Guðjónsdóttir <harpa09@ru.is>
2016-05-03 00:19:11 +0000

    Nýtt ViewModel

---
+++ Mooshak2_Hopur5/Models/ViewModels/AssignmentPartViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Mooshak2_Hopur5.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2_Hopur5.Models.ViewModels
+{
+    public class AssignmentPartViewModel
+    {
+        public int AssignmentPartId { get; set; }
+        public int AssignmentId { get; set; }
+        public string AssignmentName { get; set; }
+        public string AssignmentPartName { get; set; }
+        public string AssignmentPartDescription { get; set; }
+        public byte[] AssignmentPartFile { get; set; }
+        public double Weight { get; set; }
+        public int ProgrammingLanguageId { get; set;  }
+        public List<AssignmentPartViewModel> AssignmentPartList { get; set; }
+        public List<AssignmentTestCase> AssignmentTestCaseList { get; set; }
+    }
+}
