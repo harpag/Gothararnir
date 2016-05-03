4102fcc5516b7b9f1f9d42788bf884cee1569106
Harpa Guðjónsdóttir <harpa09@ru.is>
2016-05-03 23:43:57 +0000

    Nýjar skrá tengdar verkefni

--- Mooshak2_Hopur5/Services/AssignmentService.cs
+++ Mooshak2_Hopur5/Services/AssignmentService.cs
@@ -134,17 +134,314 @@ namespace Mooshak2_Hopur5.Services
             //Returna viewModelinu með listanum
             return viewModel;
         }
-        
 
-        //getAllAssignments()
-        //getAllAssignmentsOnSemester()
-        //getAllUserAssignments()
-        //getAllUserAssignmentsOnSemester()
-        //getAssignmentGrade()
-        //getAssignmentStatistics()
-        //editAssignment()
-        //addAssignment()
-        //addAssignmentTestCase()
+        //Sækir öll verkefni
+        public AssignmentViewModel getAllAssignments()
+        {
+            //Sæki öll gögn í verkefna töfluna
+            var assignments = (from assign in _db.Assignment
+                              join course in _db.Course on assign.courseId equals course.courseId
+                              select new { assign, course }).ToList();
+
+            //Bý til lista af verkefnum
+            List<AssignmentViewModel> assignmentsList;
+            assignmentsList = new List<AssignmentViewModel>();
+
+            //Loopa í gegnum listann úr gagnagrunninum og set inn í verkefna listann
+            foreach (var entity in assignments)
+            {
+                var assignmentParts = getAssignmentParts(entity.assign.assignmentId);
+                var result = new AssignmentViewModel
+                {
+                    AssignmentId = entity.assign.assignmentId,
+                    CourseId = entity.assign.courseId,
+                    CourseName = entity.course.courseName,
+                    CourseNumber = entity.course.courseNumber,
+                    AssignmentName =entity.assign.assignmentName,
+                    AssignmentDescription =entity.assign.assignmentDescription,
+                    AssignmentFile =entity.assign.assignmentFile,
+                    Weight = entity.assign.weight,
+                    MaxSubmission = entity.assign.maxSubmission,
+                    AssignDate = entity.assign.assignDate,
+                    DueDate = entity.assign.dueDate,
+                    GradePublished =entity.assign.gradePublished,
+                    AssignmentPartList = assignmentParts.AssignmentPartList
+                    //AssignmentSubmissionsList =
+                    //DiscussionsList =
+                 };
+                 assignmentsList.Add(result);
+            }
+
+            //Bý til nýtt AssingmentViewModel og set listann inn í það
+            AssignmentViewModel viewModel = new AssignmentViewModel
+            {
+                AssignmentList = assignmentsList
+            };
+
+            //Returna viewModelinu með listanum
+            return viewModel;
+        }
+
+        public AssignmentViewModel getAllAssignmentsOnSemester(int semesterId)
+        {
+            //Sæki öll gögn í verkefna töfluna
+            var assignments = (from assign in _db.Assignment
+                               join course in _db.Course on assign.courseId equals course.courseId
+                               join semester in _db.Semester on course.semesterId equals semester.semesterId
+                               select new { assign, course }).ToList();
+
+            //Bý til lista af verkefnum
+            List<AssignmentViewModel> assignmentsList;
+            assignmentsList = new List<AssignmentViewModel>();
+
+            //Loopa í gegnum listann úr gagnagrunninum og set inn í verkefna listann
+            foreach (var entity in assignments)
+            {
+                var assignmentParts = getAssignmentParts(entity.assign.assignmentId);
+                var result = new AssignmentViewModel
+                {
+                    AssignmentId = entity.assign.assignmentId,
+                    CourseId = entity.assign.courseId,
+                    CourseName = entity.course.courseName,
+                    CourseNumber = entity.course.courseNumber,
+                    AssignmentName = entity.assign.assignmentName,
+                    AssignmentDescription = entity.assign.assignmentDescription,
+                    AssignmentFile = entity.assign.assignmentFile,
+                    Weight = entity.assign.weight,
+                    MaxSubmission = entity.assign.maxSubmission,
+                    AssignDate = entity.assign.assignDate,
+                    DueDate = entity.assign.dueDate,
+                    GradePublished = entity.assign.gradePublished,
+                    AssignmentPartList = assignmentParts.AssignmentPartList
+                    //AssignmentSubmissionsList =
+                    //DiscussionsList =
+                };
+                assignmentsList.Add(result);
+            }
+
+            //Bý til nýtt AssingmentViewModel og set listann inn í það
+            AssignmentViewModel viewModel = new AssignmentViewModel
+            {
+                AssignmentList = assignmentsList
+            };
+
+            //Returna viewModelinu með listanum
+            return viewModel;
+        }
+
+        public AssignmentViewModel getAllUserAssignments(int userId)
+        {
+            //Sæki öll gögn í verkefna töfluna
+            var assignments = (from assign in _db.Assignment
+                               join course in _db.Course on assign.courseId equals course.courseId
+                               join userCourse in _db.UserCourse on course.courseId equals userCourse.courseId
+                               where userCourse.userId == userId
+                               select new { assign, course }).ToList();
+
+            //Bý til lista af verkefnum
+            List<AssignmentViewModel> assignmentsList;
+            assignmentsList = new List<AssignmentViewModel>();
+
+            //Loopa í gegnum listann úr gagnagrunninum og set inn í verkefna listann
+            foreach (var entity in assignments)
+            {
+                var assignmentParts = getAssignmentParts(entity.assign.assignmentId);
+                var result = new AssignmentViewModel
+                {
+                    AssignmentId = entity.assign.assignmentId,
+                    CourseId = entity.assign.courseId,
+                    CourseName = entity.course.courseName,
+                    CourseNumber = entity.course.courseNumber,
+                    AssignmentName = entity.assign.assignmentName,
+                    AssignmentDescription = entity.assign.assignmentDescription,
+                    AssignmentFile = entity.assign.assignmentFile,
+                    Weight = entity.assign.weight,
+                    MaxSubmission = entity.assign.maxSubmission,
+                    AssignDate = entity.assign.assignDate,
+                    DueDate = entity.assign.dueDate,
+                    GradePublished = entity.assign.gradePublished,
+                    AssignmentPartList = assignmentParts.AssignmentPartList
+                    //AssignmentSubmissionsList =
+                    //DiscussionsList =
+                };
+                assignmentsList.Add(result);
+            }
+
+            //Bý til nýtt AssingmentViewModel og set listann inn í það
+            AssignmentViewModel viewModel = new AssignmentViewModel
+            {
+                AssignmentList = assignmentsList
+            };
+
+            //Returna viewModelinu með listanum
+            return viewModel;
+        }
+
+        public AssignmentViewModel getAllUserAssignmentsOnSemester(int userId, int semesterId)
+         {
+            //Sæki öll gögn í verkefna töfluna
+            var assignments = (from assign in _db.Assignment
+                               join course in _db.Course on assign.courseId equals course.courseId
+                               join userCourse in _db.UserCourse on course.courseId equals userCourse.courseId
+                               join semester in _db.Semester on course.semesterId equals semester.semesterId
+                               where semester.semesterId == semesterId && userCourse.userId == userId
+                               select new { assign, course }).ToList();
+
+            //Bý til lista af verkefnum
+            List<AssignmentViewModel> assignmentsList;
+            assignmentsList = new List<AssignmentViewModel>();
+
+            //Loopa í gegnum listann úr gagnagrunninum og set inn í verkefna listann
+            foreach (var entity in assignments)
+            {
+                var assignmentParts = getAssignmentParts(entity.assign.assignmentId);
+                var result = new AssignmentViewModel
+                {
+                    AssignmentId = entity.assign.assignmentId,
+                    CourseId = entity.assign.courseId,
+                    CourseName = entity.course.courseName,
+                    CourseNumber = entity.course.courseNumber,
+                    AssignmentName = entity.assign.assignmentName,
+                    AssignmentDescription = entity.assign.assignmentDescription,
+                    AssignmentFile = entity.assign.assignmentFile,
+                    Weight = entity.assign.weight,
+                    MaxSubmission = entity.assign.maxSubmission,
+                    AssignDate = entity.assign.assignDate,
+                    DueDate = entity.assign.dueDate,
+                    GradePublished = entity.assign.gradePublished,
+                    AssignmentPartList = assignmentParts.AssignmentPartList
+                    //AssignmentSubmissionsList =
+                    //DiscussionsList =
+                };
+                assignmentsList.Add(result);
+            }
+
+            //Bý til nýtt AssingmentViewModel og set listann inn í það
+            AssignmentViewModel viewModel = new AssignmentViewModel
+            {
+                AssignmentList = assignmentsList
+            };
+
+            //Returna viewModelinu með listanum
+            return viewModel;
+        }
+
+        public AssignmentViewModel getAssignmentGrade(int userId, int assignmentId)
+        {
+            //Sæki öll gögn í verkefna töfluna
+            var assignment = (from assign in _db.Assignment
+                               join userAssignment in _db.UserAssignment on assign.assignmentId equals userAssignment.assignmentId
+                               where assign.assignmentId == assignmentId && userAssignment.userId == userId
+                               select new { assign, userAssignment }).SingleOrDefault();
+
+            UserAssignment userAssign = null; 
+            if (assignment.assign.gradePublished == 1)
+            {
+                userAssign = new UserAssignment
+                {
+                    userAssignmentId = assignment.userAssignment.userAssignmentId,
+                    userId = assignment.userAssignment.userId,
+                    userGroupId = assignment.userAssignment.userGroupId,
+                    grade = assignment.userAssignment.grade,
+                    gradeComment = assignment.userAssignment.gradeComment
+                };
+            }
+            //Bý til nýtt AssingmentViewModel og set listann inn í það
+            AssignmentViewModel viewModel = new AssignmentViewModel
+            {
+                UserAssignment = userAssign
+            };
+
+            //Returna viewModelinu með listanum
+            return viewModel;
+        }
+
+        
+        public AssignmentViewModel editAssignment(AssignmentViewModel assignmentToChange)
+        {
+            // Sæki færsluna sem á að breyta í gagnagrunninn
+            var query = (from assignment in _db.Assignment
+                         where assignment.assignmentId == assignmentToChange.AssignmentId
+                         select assignment).SingleOrDefault();
+
+            // Set inn breyttu upplýsingarnar
+            query.courseId = assignmentToChange.CourseId;
+            query.assignmentName = assignmentToChange.AssignmentName;
+            query.assignmentDescription = assignmentToChange.AssignmentDescription;
+            query.assignmentFile = assignmentToChange.AssignmentFile;
+            query.weight = assignmentToChange.Weight;
+            query.maxSubmission = assignmentToChange.MaxSubmission;
+            query.assignDate = assignmentToChange.AssignDate;
+            query.dueDate = assignmentToChange.DueDate;
+            query.gradePublished = assignmentToChange.GradePublished;
+
+            //Vista breytingar í gagnagrunn
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                // TODO
+            }
+            return assignmentToChange;
+        }
+
+        public Boolean addAssignment(AssignmentViewModel assignmentToAdd)
+        {
+            var newAssignment = new Assignment();
+
+            //setja propery-in
+            newAssignment.courseId = assignmentToAdd.CourseId;
+            newAssignment.assignmentName = assignmentToAdd.AssignmentName;
+            newAssignment.assignmentDescription = assignmentToAdd.AssignmentDescription;
+            newAssignment.assignmentFile = assignmentToAdd.AssignmentFile;
+            newAssignment.weight = assignmentToAdd.Weight;
+            newAssignment.maxSubmission = assignmentToAdd.MaxSubmission;
+            newAssignment.assignDate = assignmentToAdd.AssignDate;
+            newAssignment.dueDate = assignmentToAdd.DueDate;
+            newAssignment.gradePublished = assignmentToAdd.GradePublished;
+            //Todo setja inn öll property
+
+            try
+            {
+                //Vista ofan í gagnagrunn
+                _db.Assignment.Add(newAssignment);
+                _db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        
+        public Boolean addAssignmentTestCase(AssignmentTestCase assignmentTestCaseToAdd)
+        {
+            var newAssignmentTestCase = new AssignmentTestCase();
+            var testCaseCount = (from testCase in _db.AssignmentTestCase
+                              where testCase.assignmentPartId == assignmentTestCaseToAdd.assignmentPartId select testCase).Count();
+            //setja propery-in
+            newAssignmentTestCase.assignmentPartId = assignmentTestCaseToAdd.assignmentPartId;
+            newAssignmentTestCase.testNumber = testCaseCount + 1;
+            newAssignmentTestCase.input = assignmentTestCaseToAdd.input;
+            newAssignmentTestCase.output = assignmentTestCaseToAdd.output;
+            //Todo setja inn öll property
+
+            try
+            {
+                //Vista ofan í gagnagrunn
+                _db.AssignmentTestCase.Add(newAssignmentTestCase);
+                _db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
         //submittAssignment()
 
         //addAssignmentDiscussion()
@@ -152,5 +449,7 @@ namespace Mooshak2_Hopur5.Services
         //addAssignmentGrade()
 
         //editAssignmentGrade()
+
+        //getAssignmentStatistics() ætla að geyma þetta
     }
 }
