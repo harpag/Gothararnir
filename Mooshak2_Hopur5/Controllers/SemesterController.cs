9d6764339eb8fa103b965fe4e01071c2e26640bd
agnes15 <agnes15@ru.is>
2016-05-07 10:21:17 +0000

    var aðeins að byrja á semester

--- Mooshak2_Hopur5/Controllers/SemesterController.cs
+++ Mooshak2_Hopur5/Controllers/SemesterController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mooshak2_Hopur5.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,18 @@ namespace Mooshak2_Hopur5.Controllers
 {
     public class SemesterController : Controller
     {
+        private SemesterService _service = new SemesterService();
         // GET: Semester
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetAllSemester()
+        {
+            var viewModel = new SemesterViewModel();
+            viewModel = _service.getAllSemester();
+            return View(viewModel);
         }
     }
 }
