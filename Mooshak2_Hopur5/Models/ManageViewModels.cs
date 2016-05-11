3c8edcd19ec54c882a4703ea9bdd7e94310a296a
Harpa Guðjónsdóttir <harpa09@ru.is>
2016-05-11 23:23:18 +0000

    Að geta breytt verkefni og pörtum

--- Mooshak2_Hopur5/Models/ManageViewModels.cs
+++ Mooshak2_Hopur5/Models/ManageViewModels.cs
@@ -56,9 +56,26 @@ namespace Mooshak2_Hopur5.Models
         [Display(Name = "Confirm new password")]
         [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+        
     }
 
-   
+    public class ChangeUserPasswordViewModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string userName { get; set; }
+    }
+
+
     public class AddPhoneNumberViewModel
     {
         [Required]
@@ -84,4 +101,6 @@ namespace Mooshak2_Hopur5.Models
         public string SelectedProvider { get; set; }
         public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
     }
+
+
 }
